5ed246ff79e0440ad0d0490212a66f3306e9637e
iosdev <iosdev@Felixs-13-MBP.local>
2018-10-16 00:32:21 -0700

    First commit

---
+++ simple-calc/simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by iosdev on 10/16/18.
+//  Copyright © 2018 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
